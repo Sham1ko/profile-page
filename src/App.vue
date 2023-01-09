f68e8711787654b4efc2f5667a1890c042287d82
Shamshyrak Zholdasbek <shamshyrak.zholdasbek@gmail.com>
2023-01-09 19:03:34 +0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <script lang="ts">
-import CustomHeader from "@/components/CustomHeader.vue";
-export default {
-  components: {
-    CustomHeader,
-  },
-};
+// import CustomHeader from "@/components/CustomHeader.vue";
+// export default {
+//   components: {
+//     CustomHeader,
+//   },
+// };
 </script>
 
 <template>
   <header>
-    <custom-header />
+    <!-- <custom-header /> -->
   </header>
   <RouterView />
 </template>
